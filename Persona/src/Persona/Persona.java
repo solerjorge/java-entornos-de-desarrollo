4ed4f85edf22b51c4143f11e447843142993a939
solerjorge <113125273+solerjorge@users.noreply.github.com>
2024-03-14 16:02:57 +0100

    Jueves 14/03/2024

---
+++ Persona/src/Persona/Persona.java
@@ -0,0 +1,109 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellidos;
+
+    /**
+     * 
+     */
+    private int edad;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellidos 
+     * @param edad
+     */
+    public void Persona(String dni, String nombre, String apellidos, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Modifica el dni de la persona
+     * @param dni
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica el nombre de la persona
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getApellidos() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * Modifica los apellidos de la persona
+     * @param apellidos
+     */
+    public void setApellidos(int apellidos) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * Modifica la edad de la persona
+     * @param edad
+     */
+    public void setEdad(int edad) {
+        // TODO implement here
+    }
+
+}
